29ef3ef8fa21d0476836742b5e9c888bb9b54e89
sergey <sergey1993.08@mail.ru>
2013-08-14 02:57:04 +0300

    mark the page and add bootstrap styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Itra::Application.routes.draw do
   get "static_pages/home"
-  root :to => 'static_pages#home'
+  root to: 'static_pages#home'
+  match '/signin', to: 'static_pages#signin'
+  match '/signup', to: 'static_pages#signup'
 end

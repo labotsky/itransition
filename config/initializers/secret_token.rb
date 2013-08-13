e5ac88287cd332c61c09ca8186e722239d9e73e4
sergey <sergey1993.08@mail.ru>
2013-08-13 23:34:07 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Itra::Application.config.secret_token = '8d1bd174e807783cfa298be4d8d5fb9ee6b4b13434c82de5ffe4642e7527583adeb533f5829d4e14c4de69ab2f482a7c99d4beceb64113f6afc0591d537039ef'

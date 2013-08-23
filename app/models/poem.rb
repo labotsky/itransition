c52d4b57d5d8535fc397f8df146926a34e1a4d85
sergey <sergey1993.08@mail.ru>
2013-08-23 10:46:34 +0300

    some bags

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -3,8 +3,7 @@ class Poem < ActiveRecord::Base
 
   validates :name, presence: true
   validates :body, presence: true
-  validates :description, presence: true
- 
+  validates :description, presence: true 
 
   has_and_belongs_to_many :categories
   has_many :paragraphs
